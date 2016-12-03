85fb24254f9337935b83aafb5ebedf33ec940bec
JD Manuel <jdmanuel@zendesk.com>
2016-12-03 12:54:35 -0800

    Getting there

--- dist/fixup.js
+++ dist/fixup.js
@@ -1,3 +1,66 @@
-"use strict";
+'use strict';
 
-fixup.js;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _nodegit = require('nodegit');
+
+var _nodegit2 = _interopRequireDefault(_nodegit);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _promisifyNode = require('promisify-node');
+
+var _promisifyNode2 = _interopRequireDefault(_promisifyNode);
+
+var fse = (0, _promisifyNode2['default'])(require('fs-extra')),
+    fileName = 'commit_count.gt',
+    counterFile = '',
+    fileContent = 'hello world',
+    directoryName = 'test',
+    authorCommitter = {},
+    date = new Date(),
+    repo = undefined,
+    index = undefined,
+    oid = undefined,
+    globalConfig = undefined;
+
+fse.ensureDir = (0, _promisifyNode2['default'])(fse.ensureDir);
+
+_nodegit2['default'].Repository.open(_path2['default'].resolve('../.git')).then(function (repoResult) {
+  repo = repoResult;
+  counterFile = _path2['default'].join(repo.workdir(), '.git_thin/' + fileName);
+  return fse.ensureFile(counterFile);
+}).then(function () {
+  fse.readFile(counterFile, 'utf8', function (err, data) {
+    console.log('got here'); // => hello!
+  });
+});
+// .then(function(oidResult) {
+//   oid = oidResult
+//   return repo.getHeadCommit()
+// })
+// .then(function(commit) {
+//   let eventEmitter = commit.history()
+
+//   eventEmitter.on('commit', function(commit) {
+//     // Use commit
+//     debugger
+//   });
+
+//   eventEmitter.on('end', function(commits) {
+//     // Use commits
+//     debugger
+//   });
+
+//   eventEmitter.on('error', function(error) {
+//     // Use error
+//     debugger
+//   });
+
+//   eventEmitter.start()
+// })
+
+// return fse.writeFile(path.join(repo.workdir(), fileName), fileContent);
+//   return repo.refreshIndex()
--- src/fixup.js
+++ src/fixup.js
@@ -1,1 +1,53 @@
-fixup.js
+import Git from 'nodegit'
+import path from 'path'
+import promisify from 'promisify-node'
+
+let fse = promisify(require('fs-extra')),
+    fileName = 'commit_count.gt',
+    counterFile = '',
+    fileContent = 'hello world',
+    directoryName = 'test',
+    authorCommitter = {},
+    date = new Date(),
+    repo, index, oid, globalConfig
+
+fse.ensureDir = promisify(fse.ensureDir)
+
+Git.Repository.open(path.resolve('../.git'))
+.then(function(repoResult) {
+  repo = repoResult
+  counterFile = path.join(repo.workdir(), '.git_thin/' + fileName)
+  return fse.ensureFile(counterFile)
+})
+.then(function() {
+  fse.readFile(counterFile, 'utf8', function (err, data) {
+    console.log('got here') // => hello!
+  })
+})
+// .then(function(oidResult) {
+//   oid = oidResult
+//   return repo.getHeadCommit()
+// })
+// .then(function(commit) {
+//   let eventEmitter = commit.history()
+
+//   eventEmitter.on('commit', function(commit) {
+//     // Use commit
+//     debugger
+//   });
+
+//   eventEmitter.on('end', function(commits) {
+//     // Use commits
+//     debugger
+//   });
+
+//   eventEmitter.on('error', function(error) {
+//     // Use error
+//     debugger
+//   });
+
+//   eventEmitter.start()
+// })
+
+// return fse.writeFile(path.join(repo.workdir(), fileName), fileContent);
+//   return repo.refreshIndex()
